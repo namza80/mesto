78b9cd8229cda1b5aae4cfa095e8582c77a10213
Maxim Volsky <namza80@yandex.ru>
2020-10-02 17:39:21 +1000

    fix: update popup js textContent

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-console.log("Hello world!");
-
 const buttonOpenPopup = document.querySelector('.profile__edit-btn')
 const buttonClosePopup = document.querySelector('.popup__close-img')
 const popup = document.querySelector('.popup__edit')
@@ -11,4 +9,15 @@ const popupToggle = () => {
 buttonOpenPopup.addEventListener("click", popupToggle)
 buttonClosePopup.addEventListener("click", popupToggle)
 
+let editSaveButton = document.querySelector('.popup__input-btn')
+let newName = document.querySelector('.profile__title')
+let newJob = document.querySelector('.profile__subtitle')
 
+
+function addNewName() {
+  let nameInput = document.querySelector('.popup__input-text_type_name')
+  let jobInput = document.querySelector('.popup__input-text_type_job')
+newJob.textContent = jobInput.value
+newName.textContent = nameInput.value
+}
+editSaveButton.addEventListener('click', addNewName)
