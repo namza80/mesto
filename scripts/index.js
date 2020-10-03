a6a53d90e84c4acd8118229a89529145f647e7ea
Maxim Volsky <namza80@yandex.ru>
2020-10-04 02:23:17 +1000

    fix html && css

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,51 @@
-const buttonOpenPopup = document.querySelector('.profile__edit-btn')
-const buttonClosePopup = document.querySelector('.popup__close-img')
-const popup = document.querySelector('.popup__edit')
+const buttonOpenPopup = document.querySelector('.profile__edit-btn');
+const buttonClosePopup = document.querySelector('.popup__close-img');
+const popup = document.querySelector('.popup');
+// let editSaveForm = document.querySelector('.popup__input');
+let editSaveButton = document.querySelector('.popup__input-btn');
+let newName = document.querySelector('.profile__title');
+let newJob = document.querySelector('.profile__subtitle');
+// let nameInput = document.querySelector('.popup__input-text_type_name');
+// let jobInput = document.querySelector('.popup__input-text_type_job');
 
-const popupToggle = () => {
-  popup.classList.toggle("popup_is-opened")
+// const popupToggle = () => {
+  const popupToggle = function() {
+  popup.classList.toggle("popup_is-opened");
 }
 
-buttonOpenPopup.addEventListener("click", popupToggle)
-buttonClosePopup.addEventListener("click", popupToggle)
+buttonOpenPopup.addEventListener("click", popupToggle);
+buttonClosePopup.addEventListener("click", popupToggle);
 
-let editSaveButton = document.querySelector('.popup__input-btn')
-let newName = document.querySelector('.profile__title')
-let newJob = document.querySelector('.profile__subtitle')
+// function addNewName() {
+//   evt.preventDefault();
+//   newJob.textContent = jobInput.value;
+//   newName.textContent = nameInput.value;
+// }
+// editSaveButton.addEventListener('click', addNewName);
 
 
-function addNewName() {
-  let nameInput = document.querySelector('.popup__input-text_type_name')
-  let jobInput = document.querySelector('.popup__input-text_type_job')
-  newJob.textContent = jobInput.value
-  newName.textContent = nameInput.value
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__input');// Воспользуйтесь методом querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+												// Так мы можем определить свою логику отправки.
+												// О том, как это делать, расскажем позже.
+
+	// Находим поля формы в DOM
+	let nameInput = document.querySelector('.popup__input-text_type_name');
+	let jobInput = document.querySelector('.popup__input-text_type_job');
+
+	// Получите значение полей из свойства value
+
+	// Выберите элементы, куда должны быть вставлены значения полей
+
+	// Вставьте новые значения с помощью textContent
 }
-editSaveButton.addEventListener('click', addNewName)
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
