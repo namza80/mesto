a3858f985eed8e3fe46a34c7eee5761562ff8909
Maxim Volsky <namza80@yandex.ru>
2020-10-05 00:07:09 +1000

    fix html && css && js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,13 @@
 const buttonOpenPopup = document.querySelector('.profile__edit-btn');
 const buttonClosePopup = document.querySelector('.popup__close-img');
 const popup = document.querySelector('.popup');
-// let editSaveForm = document.querySelector('.popup__input');
+let formElement = document.querySelector('.popup__input');
 let editSaveButton = document.querySelector('.popup__input-btn');
+let popupAutoClose = document.querySelector('.popup__container');
 let newName = document.querySelector('.profile__title');
 let newJob = document.querySelector('.profile__subtitle');
-// let nameInput = document.querySelector('.popup__input-text_type_name');
-// let jobInput = document.querySelector('.popup__input-text_type_job');
+let nameInput = document.querySelector('.popup__input-text_type_name');
+let jobInput = document.querySelector('.popup__input-text_type_job');
 
 // const popupToggle = () => {
   const popupToggle = function() {
@@ -24,28 +25,26 @@ buttonClosePopup.addEventListener("click", popupToggle);
 // editSaveButton.addEventListener('click', addNewName);
 
 
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__input');// Воспользуйтесь методом querySelector()
-
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
 	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 												// Так мы можем определить свою логику отправки.
-												// О том, как это делать, расскажем позже.
+                        // О том, как это делать, расскажем позже.
 
-	// Находим поля формы в DOM
-	let nameInput = document.querySelector('.popup__input-text_type_name');
-	let jobInput = document.querySelector('.popup__input-text_type_job');
-
-	// Получите значение полей из свойства value
-
-	// Выберите элементы, куда должны быть вставлены значения полей
-
-	// Вставьте новые значения с помощью textContent
+  newName.textContent = nameInput.value;
+  newJob.textContent = jobInput.value;
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+addEventListener('submit', function() {
+  buttonClosePopup.click();
+});
+
+
+
+
+// function popupClose() {
+// buttonClosePopup.click();
+// }
