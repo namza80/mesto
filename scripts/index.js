cfe90ec783b86301e04df83ec205567d2ba76cdc
Maxim Volsky <namza80@yandex.ru>
2020-10-07 09:52:47 +1000

    refactoring index.js

--- scripts/index.js
+++ scripts/index.js
@@ -7,37 +7,41 @@ let newJob = document.querySelector('.profile__subtitle');
 let nameInput = document.querySelector('.popup__input-text_type_name');
 let jobInput = document.querySelector('.popup__input-text_type_job');
 
-
-  buttonOpenPopup.onclick = function() {
-  console.log('клик');
+function profileCopyInput() {
   nameInput.value = newName.textContent;
   jobInput.value = newJob.textContent;
-};
-
-// const popupToggle = () => {
-  const popupToggle = function() {
-  popup.classList.toggle("popup_is-opened");
-};
-
-buttonOpenPopup.addEventListener("click", popupToggle);
-buttonClosePopup.addEventListener("click", popupToggle);
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-												// Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
-
-  newName.textContent = nameInput.value;
-  newJob.textContent = jobInput.value;
-  popupToggle();
+  popupOpen()
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+const popupOpen = function() {
+  popup.classList.add("popup_is-opened");
+};
 
-// оставил для себя как пример, я ведь учусь
+const popupClose = function() {
+  popup.classList.remove("popup_is-opened");
+};
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  newName.textContent = nameInput.value;
+  newJob.textContent = jobInput.value;
+  popupClose();
+}
+
+buttonOpenPopup.addEventListener("click", popupOpen);
+buttonClosePopup.addEventListener("click", popupClose);
+formElement.addEventListener('submit', formSubmitHandler);
+buttonOpenPopup.addEventListener('click', profileCopyInput);
+
 // addEventListener('submit', function() {
 //   buttonClosePopup.click();
 // });
+
+// // const popupToggle = () => {
+//   const popupToggle = function() {
+//     popup.classList.toggle("popup_is-opened");
+//   };
+
+//   buttonOpenPopup.addEventListener("click", popupToggle);
+//   buttonClosePopup.addEventListener("click", popupToggle);
 
